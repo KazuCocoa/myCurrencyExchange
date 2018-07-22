c7b42ef1ead1246df0a3c167639bd8fd58e77cee
Kazuaki MATSUO <fly.49.89.over@gmail.com>
2018-07-22 21:34:54 +0900

    initial

---
+++ myCurrencyExchange/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  myCurrencyExchange
+//
+//  Created by kazuaki matsuo on 2018/07/22.
+//  Copyright © 2018 KazuCocoa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
