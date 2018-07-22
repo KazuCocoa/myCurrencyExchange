60e954516ce9dad9ee36a5d0bf469fcc7f37e500
Kazuaki MATSUO <fly.49.89.over@gmail.com>
2018-07-23 01:59:34 +0900

    add alamofire

--- myCurrencyExchange/ViewController.swift
+++ myCurrencyExchange/ViewController.swift
@@ -13,13 +13,29 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        UsersAPI().user(number: 1)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
 
+import Alamofire
+
+let baseURL = "https://jsonplaceholder.typicode.com"
+
+class UsersAPI {
+    func user(number: Int) {
+        Alamofire.request("\(baseURL)/users/\(number)").responseJSON { response in
+            if let json = response.result.value {
+                print("JSON: \(json)")
+            }
+
+            if let data = response.data, let utf8Text = String(data: data, encoding: .utf8) {
+                print("Data: \(utf8Text)")
+            }
+        }
+    }
+}
